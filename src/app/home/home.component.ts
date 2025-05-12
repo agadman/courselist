906c5495abcfc4c089870bcf81af168cddbab5bc
Annika Gadman <annika.gadman@gmail.com>
2025-05-12 09:05:44 -0500

    styling och sökfunktion implementerad

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,29 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Course } from '../models/course';
 import { CoursesService } from '../services/courses.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   courses = signal<Course[]>([]); 
   error = signal<string | null>(null);
+  filterValue = signal<string>('');
   coursesService = inject(CoursesService);
+
+  filteredCourses = computed(() =>
+    this.courses().filter(course => {
+      const query = this.filterValue().toLowerCase();
+      return (
+        course.code.toLowerCase().includes(query) ||
+        course.coursename.toLowerCase().includes(query)
+      );
+    })
+  );
 
   ngOnInit() {
     this.loadCourses();
